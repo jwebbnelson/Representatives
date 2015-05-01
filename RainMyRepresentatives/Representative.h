73b09532ba5fb1748feb720a52c04bf0f4c60cf1
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-04-30 19:57:53 -0600

    Initial

---
+++ RainMyRepresentatives/Representative.h
@@ -0,0 +1,13 @@
+//
+//  Representative.h
+//  RainMyRepresentatives
+//
+//  Created by Jordan Nelson on 4/30/15.
+//  Copyright (c) 2015 Jordan Nelson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Representative : NSObject
+
+@end
---
+++ RainMyRepresentatives/Representative.m
@@ -0,0 +1,13 @@
+//
+//  Representative.m
+//  RainMyRepresentatives
+//
+//  Created by Jordan Nelson on 4/30/15.
+//  Copyright (c) 2015 Jordan Nelson. All rights reserved.
+//
+
+#import "Representative.h"
+
+@implementation Representative
+
+@end
