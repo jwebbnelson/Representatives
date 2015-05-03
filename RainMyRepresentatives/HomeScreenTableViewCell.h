5aed95d2397954acdd6bad2faf0c371df90e89af
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-05-03 15:28:08 -0600

    Removed original rootViewController

---
+++ RainMyRepresentatives/HomeScreenTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeScreenTableViewCell.h
+//  RainMyRepresentatives
+//
+//  Created by Jordan Nelson on 5/3/15.
+//  Copyright (c) 2015 Jordan Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScreenTableViewCell : UITableViewCell
+
+@end
--- RainMyRepresentatives/HomeScreenViewController.h
+++ RainMyRepresentatives/HomeScreenViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  HomeScreenViewController.h
 //  RainMyRepresentatives
 //
-//  Created by Jordan Nelson on 4/29/15.
+//  Created by Jordan Nelson on 5/3/15.
 //  Copyright (c) 2015 Jordan Nelson. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HomeScreenViewController : UIViewController
 
 @end
-
